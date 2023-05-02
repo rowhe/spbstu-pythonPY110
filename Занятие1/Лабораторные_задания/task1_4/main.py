fb48f7a2f5ddbe83147a56dff7aed66721a56190
Denis <95048755+rowhe@users.noreply.github.com>
2023-05-02 09:22:48 +0300

    some tasks is done

--- Занятие1/Лабораторные_задания/task1_4/main.py
+++ Занятие1/Лабораторные_задания/task1_4/main.py
@@ -1,9 +1,9 @@
 def list_comprehension(words: list) -> list:
-    return [...]  # TODO
+    return [str.capitalize(str.lower(i)) for i in words]  # TODO
 
 
 def list_map(words: list) -> list:
-    return list(...)  # TODO
+    return list(map(str.capitalize, map(str.lower, words)))  # TODO
 
 
 def task():
