1124bc91cdd068c33dfd0df4c4edef3b62b866d2
Denis <95048755+rowhe@users.noreply.github.com>
2023-05-11 16:22:32 +0300

    lesson 3 lab 2.6 is done

--- Занятие1/Домашние_задания/task1_1/main.py
+++ Занятие1/Домашние_задания/task1_1/main.py
@@ -1,4 +1,5 @@
 if __name__ == "__main__":
     list_ = [1, 2, 3, 4, 5]
     enum = list(zip(range(len(list_)), list_))
-    pass
+    # pass
+    print(enum)
--- Занятие3/Лабораторные_задания/task2_6/main.py
+++ Занятие3/Лабораторные_задания/task2_6/main.py
@@ -6,11 +6,12 @@ def task():
     with open(filename) as f:
         json_data = json.load(f)
 
-    return ...  # отсортировать список словарей
+    return sorted(json_data, key=lambda x: x["length"])  # отсортировать список словарей
 
 
 if __name__ == "__main__":
     data = task()
     print(json.dumps(data, indent=4))
 
-    # TODO дополнительно записать отсортированный список в JSON файл
+    with open("output.json", "w") as f2:  # дополнительно записать отсортированный список в JSON файл
+        json.dump(data, f2, indent=4)
